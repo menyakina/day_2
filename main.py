f1a214e405c97337f8d82c54f41633a12e3b3b3a
menyakina <menyakinaz@mail.ru>
2022-12-01 14:21:14 +0300

    добавили методы сравнения класса Employee

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+import datetime as dt
+
+
+def str_to_date(self_date, other_date):
+    dt1 = self_date.split('.')
+    dt2 = other_date.split('.')
+    self_date = dt.date(int(dt1[2]), int(dt1[1]), int(dt1[0]))
+    other_date = dt.date(int(dt2[2]), int(dt2[1]), int(dt2[0]))
+    return self_date, other_date
+
+
 class Employee:
     def __init__(self, number, fio, bdate, oklad, on_leave=False):
         self.number = number
@@ -13,6 +24,26 @@ class Employee:
         return f"Сотрудник {self.number} {self.fio} {self.bdate}\
  оклад: {self.oklad}, в отпуске: {'да' if self.on_leave else 'нет'} "
 
+    def __lt__(self, other):      # переопределение <
+        self_bdate, other_bdate = str_to_date(self.bdate, other.bdate)
+        return self_bdate < other_bdate
 
-petrov = Employee(1, "Петров А.А.", "12.12.2000", 50000)
+    def __eq__(self, other):  # переопределение =
+        self_bdate, other_bdate = str_to_date(self.bdate, other.bdate)
+        return self_bdate == other_bdate
+
+    def __le__(self, other): # переопределение <=
+        if self.__eq__(other):
+            return True
+        if self.__lt__(other):
+            return True
+        else:
+            return False
+
+
+petrov = Employee(1, "Петров А.А.", "09.12.1997", 50000)
 print(petrov)
+ivanov = Employee(1, "Иванов А.И.", "10.02.1996", 48000)
+print(petrov <= ivanov)
+print(petrov > ivanov)
+print(petrov == ivanov)
